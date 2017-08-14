c3c2860ff26a78e4d395dca2084470dcf8d92748
J.X.Zhang <chocolatepie213@gmail.com>
2017-08-14 14:27:50 +0800

    Initial commit

---
+++ src/main/java/cn/jxzhang/Main.java
@@ -0,0 +1,12 @@
+package cn.jxzhang;
+
+/**
+ * Created on 2017-08-14 14:26
+ * <p>Title:       Main</p>
+ * <p>Description: [Description]</p>
+ *
+ * @author <a href="mailto:zhangjiaxing@ultrapower.com.cn">zhangjiaxing</a>
+ * @version 1.0
+ */
+public class Main {
+}
